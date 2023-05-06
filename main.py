aaaaa92be7354c8fcd8c26331fec9a76487f2539
Brandon Powell <14297@coderacademy.edu.au>
2023-05-06 14:25:09 +1000

    added a home screen

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 from quiz_functions import start_quiz
 
 def main_menu():
-    pass
+    while True:
+        print("Welocme to this mutliple choice quiz game!!!")
+        print("Choose an option:")
+        print("1) Start Movie Quiz")
+        print("2) Exit Quiz")
+        choice = input("Enter your choice (1-2): ")
+        if choice == "1":
+            play_again = start_quiz()
+            if not play_again:
+                print("Thank you for playing the game! Hope to see you back soon!!")
+            continue
+        elif choice == "2":
+            print("Thank you for playing hope to see you soon!!!")
+            continue
+        else:
+            print("invalid choice, please only enter 1 or 2.")
+
+if __name__ == "__main__":
+    main_menu()
