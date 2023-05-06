a818da6f60f09e6bec780aa8852d6c115c5c181b
Brandon Powell <14297@coderacademy.edu.au>
2023-05-07 09:54:47 +1000

    fixed spelling and added an input for name

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from quiz_functions import start_quiz
 
 def main_menu():
     while True:
-        print("Welocme to this mutliple choice quiz game!!!")
+        print("Welcome to this mutliple choice quiz game!!!")
         print("Choose an option:")
         print("1) Start Movie Quiz")
         print("2) Exit Quiz")
         choice = input("Enter your choice (1-2): ")
         if choice == "1":
-            play_again = start_quiz()
+            name = input("Enter your name: ")
+            play_again = start_quiz(name)
             if not play_again:
                 print("Thank you for playing the game! Hope to see you back soon!!")
             continue
