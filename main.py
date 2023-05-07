1597f7c4224a4a26a9b304898cc14aad9e2d7853
Brandon Powell <14297@coderacademy.edu.au>
2023-05-07 15:02:33 +1000

    added view highscore and functions

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from quiz_functions import start_quiz
+from quiz_functions import start_quiz, view_highscore
 
 def main_menu():
     while True:
         print("Welcome to this mutliple choice quiz game!!!")
         print("Choose an option:")
         print("1) Start Movie Quiz")
-        print("2) Exit Quiz")
-        choice = input("Enter your choice (1-2): ")
+        print("2) View HighScore")
+        print("3) Exit Quiz")
+        choice = input("Enter your choice (1-3): ")
         if choice == "1":
             name = input("Enter your name: ")
             play_again = start_quiz(name)
@@ -14,6 +15,14 @@ def main_menu():
                 print("Thank you for playing the game! Hope to see you back soon!!")
             continue
         elif choice == "2":
+            highscores = view_highscore()
+            if highscores:
+                print("High Scores!")
+                for i, (name,score) in enumerate(highscores):
+                    print(f"{i+1}. {name} - {score}")
+            else:
+                print("No highscore just yet be the first!!!!")
+        elif choice == "3":
             print("Thank you for playing hope to see you soon!!!")
             break
         else:
