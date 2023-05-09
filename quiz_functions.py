b212a31eb96a84f5d2689de8655d3e052c83af89
Brandon Powell <14297@coderacademy.edu.au>
2023-05-09 12:43:17 +1000

    changed highscore an store quiz score base on quiz

--- quiz_functions.py
+++ quiz_functions.py
@@ -3,15 +3,18 @@ import random
 class QuizFormatError(Exception):
     pass
 
-def start_quiz(name):
+movie_score = {}
+cartoon_score = {}
+
+def start_quiz(name, questions_file, answers_file, quiz_type):
     while True:
     
         # opens question file and reads questions
-        with open("questions.txt", "r") as f:
+        with open(questions_file, "r") as f:
             quiz_questions = [line.strip() for line in f.readlines()]
         
         # open the answers file and read the answers into a dictionary
-        with open("answers.txt", "r") as f:
+        with open(answers_file, "r") as f:
             quiz_answers = {}
         
             for line in f:
@@ -28,14 +31,14 @@ def start_quiz(name):
         score = 0
 
         # Display welcome message
-        print(f"Welcome {name} to this Quiz! Good Luck!!!!!!\n")
+        print(f"Welcome {name} to the Quiz! Good Luck!!!!!!\n")
 
         # ask question in order
         for i, question in enumerate(quiz_questions):
             try:
                 # check if there is a colon in line 
                 if ":" in question:
-                # Split questions and choices
+                    # Split questions and choices
                     question, choices = question.split(":")
                     choices = choices.split(",")
                     # Shuffle answer choices
@@ -55,9 +58,10 @@ def start_quiz(name):
                 # Answers is incorrect it will check
                 if choices[int(answer)-1] == correct_answer:
                     print("Whoop Whoop Correct!!!\n")
-                    score += 1
+                    score += 1                    
                 else:
                     print(f"OH NO Incorrect!!!! Correct answer is: {correct_answer}\n")
+                    break # break loop if answer is wrong
             # Except the error and will continue
             except UnboundLocalError:
                 print("Error: 'choice' varibale not defined.")
@@ -67,10 +71,16 @@ def start_quiz(name):
             except Exception as e:
                 print(f"Error: {e}")
 
+        # Based on quiz will store scores
+        if quiz_type == "movie":
+            movie_score[name] = score
+        elif quiz_type == "cartoon":
+            cartoon_score[name] = score
+
             # print the final score
         print(f"Your score is {score}/{len(quiz_questions)}.")  
         with open("highscores.txt", "a") as f:
-            f.write(f"{name}:{score}\n")
+            f.write(f"{name}:{score}:{quiz_type}\n")
 
     # If "y" will play again or if "n" will return to main menu      
         while True:
@@ -85,17 +95,26 @@ def start_quiz(name):
             else:
                 print("Invalid Input. Please choose y or n ")
 
-def view_highscore():
+def view_highscore(quiz_type):
         try:
             with open("highscores.txt", "r") as f:
                 highscores = [line.strip().split(":") for line in f.readlines()]
+        # except file not found and will execute the file 
         except FileNotFoundError:
             highscores = []
 
-        # returns score as a list of tuples
-        return highscores     
-
         
+        if highscores:
+            quiz_scores = {}
+        for line in highscores:
+            if len(line) == 3 and line[2] == quiz_type:
+                name, score, qtype = line
+                quiz_scores[name] = int(score)
+            
+        if quiz_scores:
+            sorted_scores = sorted(quiz_scores.items(), key=lambda x: x[1], reverse=True)
+            return sorted_scores
+        return []
 
 
         
