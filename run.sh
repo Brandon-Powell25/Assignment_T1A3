e12160e2ca1bfd083e3e7fe1530da48eec45be93
Brandon Powell <14297@coderacademy.edu.au>
2023-05-14 13:59:20 +1000

    added comments above text

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Virtual environment is created for the quiz
 python3 -m venv quiz-venv
+# Activate the virtual environment
 source quiz-venv/bin/activate
+# Installs all packages that are need from requirements.txt
 pip3 install -r requrirements.txt
+# Clears the terminal screen for you
 clear
+# runs the main.py to start the quiz
 python3 main.py
